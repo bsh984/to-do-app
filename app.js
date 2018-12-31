88dc1c14c091a3ee1df74365f37d125a61ad67db
Bells <bellen.shen.designs@gmail.com>
2018-12-31 17:02:49 -0500

    assignment work - does not work. am stuck

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 function onReady() {
   //declare toDos as blank array
   const toDos = [];
-  //assign ID to var addToDoForm. now can use addToDoForm and manipulate
+  let id=0;
+
   const addToDoForm = document.getElementById('addToDoForm');
 
   //declare createNewToDo function
@@ -10,15 +11,18 @@ function onReady() {
     const newToDoText = document.getElementById('newToDoText');
     if (!newToDoText.value) { return; }
 
-    // create new array object each time createNewToDo is executed
+    // note to self: create new array object each time createNewToDo is executed
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id.value
     });
+
+    id++;
     // set newToDoText value used by the field to empty
     newToDoText.value = '';
 
-    // need this here to update the UI based on changes in state
+    // note to self: need this here to update the UI based on changes in state
     // since the event handler below will preventDefault submit behavior
     // otherwise the renderTheUI will never get called
     renderTheUI();
@@ -28,16 +32,29 @@ function onReady() {
     const toDoList = document.getElementById('toDoList');
 
     toDoList.textContent = '';
-    
+
     toDos.forEach(function(toDo){
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
+      const delButton = document.createElement('button');
+      button.type = "button";
+
+
+      // for assignment checker - i'm stuck here... how do I use the filter technique?
+      delButton.addEventListener('click',event => {
+        toDos = toDos.filter(toDo => toDo.id==id) {
+          return toDo.id !== id;
+        })
+        renderTheUI();
+      });
+
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(delButton);
     });
   }
 
